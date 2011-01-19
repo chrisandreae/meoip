a1f260af9fdc783d42830daae8c631afb6bb42b3
nuclearcat@gmail.com <nuclearcat@gmail.com@5c11c7ec-281c-4ebd-d45a-545a75e6ed38>
2011-01-19 21:28:54 +0000

    FreeBSD support and multiple tunnels by Daniil Kharun
    git-svn-id: http://linux-eoip.googlecode.com/svn/trunk@9 5c11c7ec-281c-4ebd-d45a-545a75e6ed38

--- meoip.c
+++ meoip.c
@@ -1,6 +1,8 @@
 /*
     file:   meoip.c
-    Author: Denys Fedoryshchenko aka NuclearCat <nuclearcat@nuclearcat.com>
+    Authors: 
+    Linux initial code: Denys Fedoryshchenko aka NuclearCat <nuclearcat (at) nuclearcat.com>
+    FreeBSD support: Daniil Kharun <harunaga (at) harunaga.ru>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,7 +27,10 @@
 #include <arpa/inet.h>
 #include <netinet/if_ether.h>
 #include <net/ethernet.h>
+#ifdef __linux__
 #include <netinet/ether.h>
+#include <linux/if_tun.h>
+#endif
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <string.h>
@@ -35,91 +40,249 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
-#include <linux/if_tun.h>
+#ifdef __FreeBSD__
+#include <net/if_tap.h>
+#endif
 #include <net/if.h>
 #include <poll.h>
 #include <unistd.h>
-
+#include <err.h>
+#include <signal.h>
 
 #define MAXPAYLOAD (65536)
+#define TUN_MAX_TRY 50
+
+/*! Assert*/
+#define assert(x, f) \
+if  (x == NULL) \
+  { warn("%s:%d %s: %m", __FILE__, __LINE__, f); exit(1);}
+
+
+
+typedef struct
+{
+   struct sockaddr_in   daddr;
+   int                  id;
+   int                  fd;
+   struct ifreq		ifr;
+}  Tunnel;
+
+int numtunnels;
+Tunnel *tunnels;
+
+
+void term_handler(int s)
+{ 
+  int 		fd, i;
+  Tunnel	*tunnel;
+
+  if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("socket() failed");
+    exit(-1);
+  }
+
+  for(i=0;i<numtunnels;i++)
+  {
+    tunnel = tunnels + i;
+
+#ifdef __FreeBSD__
+    if (ioctl(fd, SIOCIFDESTROY, &tunnel->ifr))
+    {
+      perror( "ioctl(SIOCIFDESTROY) failed");
+    }
+#endif
+    close(tunnel->fd);
+  }
+
+  close(fd);
+  exit(0);
+}
+
+#ifdef __linux__
+int open_tun(Tunnel *tunnel)
+{
+    int fd;
+    
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+      perror("socket() failed");
+      return 1;
+    }
+    if ( (tunnel->fd = open("/dev/net/tun",O_RDWR)) < 0)
+    {
+	perror("open_tun: /dev/net/tun error");
+	return 1;
+    }
+
+    bzero(&tunnel->ifr, sizeof(tunnel->ifr));
+
+    tunnel->ifr.ifr_flags = IFF_TAP|IFF_NO_PI;
+    strncpy(tunnel->ifr.ifr_name, "eoip%d",IFNAMSIZ);
+    if (ioctl(tunnel->fd, TUNSETIFF, (void *)&tunnel->ifr) < 0) {
+        perror("ioctl-1");
+        close(fd);
+        return 1;
+    }
+    if (ioctl(tunnel->fd, TUNGETIFF, (void *)&tunnel->ifr) < 0) {
+        perror("ioctlg-1");
+        close(fd);
+        return 1;
+    }
+
+    tunnel->ifr.ifr_flags |= IFF_UP;
+    tunnel->ifr.ifr_flags |= IFF_RUNNING;
+
+    if (ioctl(fd, SIOCSIFFLAGS, (void *)&tunnel->ifr) < 0) {
+        perror("ioctl-2");
+        close(fd);
+        return 1;
+    }
+    close(fd);
+    return 0;
+}
+
+#endif
+
+#ifdef __FreeBSD__
+
+int open_tun(Tunnel *tunnel)
+{
+    int fd;
+
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+      perror("socket() failed");
+      return 1;
+    }
+
+    if ((tunnel->fd = open("/dev/tap", O_RDWR)) < 0) 
+    {
+	perror("open_tun: /dev/tap error");
+	return 1;
+    }
+
+    bzero(&tunnel->ifr, sizeof(tunnel->ifr));
+
+    if (ioctl(tunnel->fd, TAPGIFNAME, &tunnel->ifr)) 
+    {
+      perror( "ioctl(TAPGIFNAME) failed");
+      close(fd);
+      return 1;
+    }
+
+    tunnel->ifr.ifr_name[IFNAMSIZ-1] = 0;
+    tunnel->ifr.ifr_flags = IFF_UP | IFF_RUNNING;
+
+    if (ioctl(fd, SIOCSIFFLAGS, &tunnel->ifr)) 
+    {
+      perror( "ioctl(SIOCSIFFLAGS) failed");
+      close(fd);
+      return 1;
+    }
+
+    close(fd);
+    return 0;
+}
+#endif
 
 int main(int argc,char **argv)
-{ 
+{
     int raw_socket = socket(PF_INET, SOCK_RAW, 47);
     int payloadsz;
     unsigned char *ip = malloc(MAXPAYLOAD+8); /* 8-byte header of GRE, rest is payload */
     unsigned char *rcv = malloc(MAXPAYLOAD+28); /* Header on receive is larger */
-    struct sockaddr_in daddr;    
+    struct sockaddr_in daddr;
     unsigned char *payloadptr = ip+8;
-    int fd,ret;
-    struct ifreq ifr;
-    struct pollfd pollfd[2];
-    int tunnelid;
-    char devname[IFNAMSIZ];
+    int ret, i;
+    struct pollfd pollfd[argc-1];
+    Tunnel *tunnel;
+    struct sigaction sa;
 
     printf("Mikrotik EoIP %s\n",VERSION);
     printf("(c) Denys Fedoryshchenko <nuclearcat@nuclearcat.com>\n");
 
-    if(argc < 2){ 
-        fprintf(stdout,"Usage: %s tunnelid peerip\n",argv[0]);
+    if(argc < 2 || argc > TUN_MAX_TRY ){
+        fprintf(stdout,"Usage: %s peerip/tunnelid ...\n",argv[0]);
         return 0;
     }
-    tunnelid = atoi(argv[1]);
+
+    numtunnels=argc-1;
+    tunnels = malloc(sizeof(Tunnel)*numtunnels);
+    assert(tunnels, "malloc()");
+    bzero(tunnels,sizeof(Tunnel)*numtunnels);
+
+    {
+
+
+    for(i=0;i<numtunnels;i++)
+    {
+      int strln, arg_position=0, j;
+      char *s;
+      j=0;
+      tunnel = tunnels + i;
+      strln = strlen(argv[i+1]) + 1;
+      s = argv[i+1];
+      for(j=0;j<strln;j++)
+      {
+        if ( *(argv[i+1]+j) == '/' || *(argv[i+1]+j) == 0x00)
+        {
+          *(argv[i+1]+j) = 0x0;
+          arg_position++;
+          switch (arg_position)
+          {
+            case 1:
+              tunnel->daddr.sin_family = AF_INET;
+              tunnel->daddr.sin_port = 0;
+    	      if (!inet_pton(AF_INET, s, (struct in_addr *)&tunnel->daddr.sin_addr.s_addr))
+	      {
+	        warn("Destination \"%s\" is not correct\n", s);
+	        exit(-1);
+	      }
+	      bzero(tunnel->daddr.sin_zero, sizeof(tunnel->daddr.sin_zero));
+              s = argv[i+1]+j+1;
+            break;
+            case 2:
+	      tunnel->id = atoi(s);
+	      if ( !tunnel->id )
+	      {
+                warn("Tunnel id \"%s\" is not correct\n", s);
+                exit(-1);
+	      }
+              s = argv[i+1]+j+1;
+            break;
+          }
+        }
+      }
+      if (arg_position!=2)
+      {
+        fprintf(stdout,"Usage: %s peerip/tunnelid ...\n",argv[0]);
+        exit(-1);
+      }
+    }
+
+    }
 
     if (raw_socket == -1) {
 	perror("raw socket error():");
 	exit(-1);
     }
 
+    bzero(ip,20);
 
-    daddr.sin_family = AF_INET;
-    daddr.sin_port = 0;
-    if (!inet_pton(AF_INET, argv[2], (struct in_addr *)&daddr.sin_addr.s_addr))
+    daemon(0,1);
+
+    for(i=0;i<numtunnels;i++)
     {
-	printf("Destination is not correct\n");
-	exit(-1);
-    }
-    memset(daddr.sin_zero, 0, sizeof(daddr.sin_zero));
+      tunnel = tunnels + i;
 
-    memset(ip,0x0,20);
-
-    /* Tun */
-    if ( (fd = open("/dev/net/tun",O_RDWR)) < 0) 
-	perror("open /dev/net/tun");
-
-    memset(&ifr, 0, sizeof(ifr));
-    ifr.ifr_flags = IFF_TAP|IFF_NO_PI;
-    strncpy(ifr.ifr_name, "eoip%d",IFNAMSIZ);
-    if (ioctl(fd, TUNSETIFF, (void *)&ifr) < 0) {
-	perror("ioctl-1");
-	exit(-1);
+      if ( open_tun(tunnel) ) {
+        exit(-1);
+      }
     }
 
-    if (ioctl(fd, TUNGETIFF, (void *)&ifr) < 0) {
-	perror("ioctlg-1");
-	exit(-1);
-    }
-    strncpy(devname,ifr.ifr_name,IFNAMSIZ);
-
-
-    ifr.ifr_flags |= IFF_UP;
-    ifr.ifr_flags |= IFF_RUNNING;
-    strncpy(ifr.ifr_name, devname,IFNAMSIZ);
-    if (ioctl(raw_socket, SIOCSIFFLAGS, (void *)&ifr) < 0) {
-	perror("ioctl-2");
-	exit(-1);
-    }
-
-
-//    memset(&ifr, 0, sizeof(ifr));
-//    ifr.ifr_mtu = 1492;
-//    strncpy(ifr.ifr_name, devname,IFNAMSIZ);
-//    if (ioctl(raw_socket, SIOCSIFMTU, (void *)&ifr) < 0) {
-//	perror("ioctl-3");
-//	exit(-1);
-//    }
-
-
+    bzero(&sa, sizeof(sa));
+    sa.sa_handler = term_handler;
+    sigaction( SIGTERM , &sa, 0);
+    sigaction( SIGINT , &sa, 0);
+    sleep(100);
 
     /* structure of Mikrotik EoIP:
 	... IP header ...
@@ -134,51 +297,72 @@ int main(int argc,char **argv)
     ip[2] = 0x64;
     ip[3] = 0x00;
 
-    // tunnel id
-    printf("Peer %s TunnelID %d\n",argv[2],tunnelid);
-    ip[6] = (unsigned char )(tunnelid & 0xFF);
-    ip[7] = (unsigned char )(((tunnelid & 0xFF00) >> 8));
-
     //ip[6] = 0xd2;
     //ip[7] = 0x04;
 
-    pollfd[0].fd = fd;
+
+
+    pollfd[0].fd = raw_socket;
     pollfd[0].events = POLLIN;
     pollfd[0].revents = 0; /* unneccesary? */
 
-    pollfd[1].fd = raw_socket;
-    pollfd[1].events = POLLIN;
-    pollfd[1].revents = 0; /* unneccesary? */
+    for(i=0;i<numtunnels;i++)
+    {
+      tunnel=tunnels + i;
+      
+      pollfd[i+1].fd = tunnel->fd;
+      pollfd[i+1].events = POLLIN;
+      pollfd[i+1].revents = 0; /* unneccesary? */
+    }
 
-    daemon(0,0);
-
-    while ((ret = poll(pollfd,2,-1)) >= 0) {
+    while ((ret = poll(pollfd,numtunnels+1,-1)) >= 0) {
 	if (pollfd[0].revents) {
-	    pollfd[0].revents=0;
-	    payloadsz = read(fd,payloadptr,MAXPAYLOAD);
-	    if (payloadsz < 0)
-		break;
-	    ip[4] = (unsigned char)(payloadsz & 0xFF00) << 8;
-	    ip[5] = (unsigned char)(payloadsz & 0xFF);
-	    if(sendto(raw_socket, ip, payloadsz+8, 0,(struct sockaddr *)&daddr, (socklen_t)sizeof(daddr)) < 0)
-		perror("send() err");	
-	}
-
-	if (pollfd[1].revents) {
 	    /* TODO: verify tunnel id */
 	    payloadsz = read(raw_socket,rcv,MAXPAYLOAD);
 	    if (payloadsz < 28)
 		continue;
-	    if (rcv[26] == (unsigned char )(tunnelid & 0xFF) && rcv[27] == (unsigned char )(((tunnelid & 0xFF00) >> 8))) {
-		/* TODO: verify tunnel id */
-		
+
+            for(i=0;i<numtunnels;i++)
+            {
+	      tunnel=tunnels + i;
+              /* TODO: verify tunnel id */
+              if (rcv[26] == (unsigned char )(tunnel->id & 0xFF) && rcv[27] == (unsigned char )(((tunnel->id & 0xFF00) >> 8)))
+	      {
 		if (payloadsz<0)
 		    break;
 		if (payloadsz>8) {
-		    write(fd,rcv+28,payloadsz-28);
+		    write(tunnel->fd,rcv+28,payloadsz-28);
 		}
+		break;
+	      }
 	    }
+	    continue;
 	}
+
+
+        for(i=0;i<numtunnels;i++)
+        {
+          tunnel=tunnels + i;
+
+	  if (pollfd[i+1].revents)
+	  {
+	    pollfd[i+1].revents=0;
+	    payloadsz = read(tunnel->fd,payloadptr,MAXPAYLOAD);
+	    if (payloadsz < 0)
+		break;
+	    ip[4] = (unsigned char)(payloadsz & 0xFF00) << 8;
+	    ip[5] = (unsigned char)(payloadsz & 0xFF);
+
+	    // tunnel id
+	    ip[6] = (unsigned char )(tunnel->id & 0xFF);
+	    ip[7] = (unsigned char )(((tunnel->id & 0xFF00) >> 8));
+
+	    if(sendto(raw_socket, ip, payloadsz+8, 0,(struct sockaddr *)&tunnel->daddr, (socklen_t)sizeof(daddr)) < 0)
+		perror("send() err");
+	    break;
+	  }
+	}
+
     }
     return(0);
 }
